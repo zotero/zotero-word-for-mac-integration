483805b0b9ae27c54a35d7b11714366b91907000
Simon Kornblith <simon@simonster.com>
2011-03-22 15:18:32 +0000

    update build script

--- build/build_appscript.sh
+++ build/build_appscript.sh
@@ -2,13 +2,17 @@
 cd appscript-*
 
 rm -rf build
-export CFLAGS="-arch ppc -arch i386 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.5.sdk"
+export CC=gcc-4.0
+export CXX=gcc-4.0
+export CFLAGS="-arch ppc -arch i386 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="$CFLAGS"
 /System/Library/Frameworks/Python.framework/Versions/2.5/bin/python setup.py build
 mv build/lib*/aem/ae.so ae-ppc.so
 
 rm -rf build
+export CC=gcc-4.2
+export CXX=gcc-4.2
 export CFLAGS="-arch x86_64 -mmacosx-version-min=10.5 -isysroot /Developer/SDKs/MacOSX10.5.sdk"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="$CFLAGS"
