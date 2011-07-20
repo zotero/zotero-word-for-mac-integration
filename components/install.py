f76fe9542d269eb49caaff5ca82516edd150e59c
Simon Kornblith <simon@simonster.com>
2011-07-20 23:53:39 +0000

    Fix installation issue on Lion

--- components/install.py
+++ components/install.py
@@ -119,7 +119,7 @@ class Installer:
 				
 				# Generate scripts
 				for i in range(len(W2008_SCRIPT_NAMES)):
-					p = subprocess.Popen(['/usr/bin/osacompile', '-o', scriptPath+W2008_SCRIPT_NAMES[i]],
+					p = subprocess.Popen(['/usr/bin/osacompile', '-t', 'osas', '-c', 'ToyS', '-o', scriptPath+W2008_SCRIPT_NAMES[i]],
 						stdin=subprocess.PIPE)
 					p.stdin.write(SCRIPT_TEMPLATE.safe_substitute(command=W2008_SCRIPT_COMMANDS[i]))
 					p.stdin.close()
