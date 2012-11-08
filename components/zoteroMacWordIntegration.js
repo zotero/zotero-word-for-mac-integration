zotero/zotero-word-for-mac-integration

commit d094ef2ab4b481ff928ea5b9ece35c81d7a01435

freeString -> freeData

components/zoteroMacWordIntegration.js
@@ -179,8 +179,8 @@ function init() {
 		"writeScript":lib.declare("writeScript", ctypes.default_abi, statusCode, ctypes.char.ptr,
 			ctypes.char.ptr),
 		
-		// statusCode freeString(char* string);
-		"freeString":lib.declare("freeString", ctypes.default_abi, statusCode, ctypes.char.ptr)
+		// statusCode freeData(void* ptr);
+		"freeData":lib.declare("freeData", ctypes.default_abi, statusCode, ctypes.void_t.ptr)
 	};
 	
 	fieldPtr = new ctypes.PointerType(field_t);
@@ -246,7 +246,7 @@ Installer.prototype = {
 		var returnValue = new ctypes.char.ptr();
 		checkStatus(f.getScriptItemsDirectory(returnValue.address()));
 		var outString = returnValue.readString();
-		f.freeString(returnValue);
+		f.freeData(returnValue);
 		return outString;
 	},
 	"writeScript":function(scriptPath, scriptContent) {
@@ -343,7 +343,9 @@ Document.prototype = {
 		checkIfFreed(this._documentStatus);
 		var returnValue = new ctypes.char.ptr();
 		checkStatus(f.getDocumentData(this._document_t, returnValue.address()));
-		return returnValue.readString();
+		var data = returnValue.readString();
+		f.freeData(returnValue);
+		return data;
 	},
 	
 	"setDocumentData":function(documentData) {
@@ -504,7 +506,7 @@ Field.prototype = {
 		checkIfFreed(this._documentStatus);
 		var returnValue = new ctypes.char.ptr();
 		checkStatus(f.getText(this._field_t, returnValue.address()));
-		return returnValue.readString();
+		returnValue.readString();
 	},
 	
 	"setCode":function(code) {

build/src/utilities.m
@@ -160,9 +160,9 @@ char* copyNSString(NSString* string) {
 	return newString;
 }
 
-// Escapes a C string for use with AppleScript
-void freeString(char* string) {
-	free(string);
+// Frees a C string
+void freeData(void* ptr) {
+	free(ptr);
 }
 
 // Generates a random string.

build/src/zoteroMacWordIntegration.h
@@ -214,7 +214,7 @@ NSString* getTemporaryFilePath(void);
 NSString* posixPathToHFSPath(NSString *posixPath);
 
 char* copyNSString(NSString* string);
-void freeString(char* string);
+void freeData(void* ptr);
 NSString* generateRandomString(NSUInteger length);
 NSInteger getEntryIndex(document_t* x, SBObject* y);
 

commit 0dbe77d4c9e198c3ffae2cbe9869bcc707d6c9fd

Fix getText() call (broken by d094ef2ab4b481ff928ea5b9ece35c81d7a01435)

components/zoteroMacWordIntegration.js
@@ -506,7 +506,7 @@ Field.prototype = {
 		checkIfFreed(this._documentStatus);
 		var returnValue = new ctypes.char.ptr();
 		checkStatus(f.getText(this._field_t, returnValue.address()));
-		returnValue.readString();
+		return returnValue.readString();
 	},
 	
 	"setCode":function(code) {