zotero/zotero-word-for-mac-integration

commit 4a806726be029cf799a02fe7c3693a6aa3855429

Hopefully final fix for cursor not being placed after inserted citation
Stores entry index of inserted field and restores cursor to after the
field upon operation finalization

build/src/utilities.m
@@ -110,7 +110,7 @@ NSString* tempFileStringNS = nil;
 FILE* getTemporaryFile(document_t *doc) {
 	if(tempFile == NULL) {
         const char *tempFileTemplate;
-        if(doc->wordVersion == 2016 || doc->wordVersion == 16) {
+        if(doc->wordVersion == 2016 || doc->wordVersion >= 16 && doc->wordVersion < 2000) {
             tempFileTemplate = [[NSTemporaryDirectory()
                                              stringByAppendingPathComponent:
                                              @"com.microsoft.Word/zotero.XXXXXX.rtf"]
@@ -178,7 +178,7 @@ void freeData(void* ptr) {
 }
 
 void storeCursorLocation(document_t* doc) {
-	if (doc->wordVersion == 16) {
+	if (doc->wordVersion >= 16 && doc->wordVersion < 2000) {
 		doc->restoreCursorEnd = doc->restoreNote = -1;
 		WordE160 storyType = [[doc->sbApp selection] storyType];
 		if (storyType == WordE160EndnotesStory || storyType == WordE160FootnotesStory) {
@@ -189,13 +189,17 @@ void storeCursorLocation(document_t* doc) {
 				doc->restoreNote = getEntryIndex(doc, [[[doc->sbApp selection] endnotes] objectAtIndex:0])-1;
 			}
 		} else {
-			doc->restoreCursorEnd = [[doc->sbApp selection] selectionEnd];
+			if ([[[doc->sbApp selection] fields] count]) {
+				doc->restoreFieldIdx = getEntryIndex(doc, [[[doc->sbApp selection] fields] objectAtIndex:1]);
+			} else {
+				doc->restoreCursorEnd = [[doc->sbApp selection] selectionEnd];
+			}
 		}
 	}
 }
 
 statusCode moveCursorOutOfNote(document_t* doc) {
-	if (doc->wordVersion == 16) {
+	if (doc->wordVersion >= 16 && doc->wordVersion < 2000) {
 		WordE160 storyType = [[doc->sbApp selection] storyType];
 		if (storyType == WordE160EndnotesStory || storyType == WordE160FootnotesStory) {
 			WordTextRange* noteSelection;
@@ -228,9 +232,13 @@ statusCode restoreCursor(document_t* doc) {
 			CHECK_STATUS;
 		}
 		doc->restoreNote = -1;
+	} else if (doc->restoreFieldIdx != -1) {
+		long position = [[[[doc->sbDoc fields] objectAtIndex:doc->restoreFieldIdx-1] resultRange] endOfContent]+1;
+		[[doc->sbDoc createRangeStart:position end:position]
+		 sendEvent:'misc' id:'slct' parameters:'\00\00\00\00', nil];
 	} else if (doc->restoreCursorEnd != -1) {
 		[[doc->sbDoc createRangeStart:doc->restoreCursorEnd end:doc->restoreCursorEnd]
-						   sendEvent:'misc' id:'slct' parameters:'\00\00\00\00', nil];
+		 sendEvent:'misc' id:'slct' parameters:'\00\00\00\00', nil];
 		CHECK_STATUS;
 	}
 	doc->cursorMoved = NO;

build/src/application.m
@@ -36,7 +36,7 @@ statusCode getDocument(int wordVersion, const char* wordPath,
 	doc->allocatedFieldsEnd = NULL;
 	doc->allocatedFieldListsStart = NULL;
 	doc->allocatedFieldListsEnd = NULL;
-	doc->restoreNote = doc->restoreCursorEnd = -1;
+	doc->restoreNote = doc->restoreCursorEnd = doc->restoreFieldIdx = -1;
 	doc->cursorMoved = NO;
 	
 	// Get application by path if a path has been specified

build/src/document.m
@@ -1414,7 +1414,6 @@ statusCode insertFieldRaw(document_t *doc, const char fieldType[],
 		CHECK_STATUS
 		[sbSelection setSelectionEnd:[sbNoteReference endOfContent]];
 		CHECK_STATUS
-		storeCursorLocation(doc);
 		
 		sbWhere = [sbNote textObject];
 	} else if(storyType == WordE160FootnotesStory) {
@@ -1444,13 +1443,9 @@ statusCode insertFieldRaw(document_t *doc, const char fieldType[],
 		// The above version works for most users of 16.9+, but some report
 		// the field being inserted at cursor and not at the textRange,
 		// which then breaks further actions so we move the cursor to the insertion point
-		if (doc->wordVersion == 16) {
+		if (doc->wordVersion >= 16 && doc->wordVersion < 2000) {
 			[sbWhere sendEvent:'misc' id:'slct' parameters:'\00\00\00\00', nil];
-			if (sbNote) {
-				doc->cursorMoved = true;
-			} else {
-				storeCursorLocation(doc);
-			}
+			doc->cursorMoved = YES;
 			CHECK_STATUS
 		}
 		
@@ -1539,8 +1534,11 @@ statusCode insertFieldRaw(document_t *doc, const char fieldType[],
 		}
 		
 		if(returnValue) {
-			return initField(doc, sbField, noteType, entryIndex, YES,
-							 returnValue);
+			ENSURE_OK(initField(doc, sbField, noteType, entryIndex, YES,
+							 returnValue));
+			doc->restoreFieldIdx = getEntryIndex(doc, (*returnValue)->sbField);
+			CHECK_STATUS
+			return STATUS_OK;
 		}
 		
 		return STATUS_OK;

build/src/zoteroMacWordIntegration.h
@@ -156,6 +156,7 @@ typedef struct Document {
 	NSInteger restoreNote;
 	WordE160 restoreNoteType;
 	NSInteger restoreCursorEnd;
+	NSInteger restoreFieldIdx;
 	BOOL cursorMoved;
 	
 	listNode_t* allocatedFieldsStart;

commit 526b278a14af76b56d65da4400638b39fdfe0dbf

Fix an obob when citation is fully within a selection
Regression since 4a806726

build/src/utilities.m
@@ -190,7 +190,7 @@ void storeCursorLocation(document_t* doc) {
 			}
 		} else {
 			if ([[[doc->sbApp selection] fields] count]) {
-				doc->restoreFieldIdx = getEntryIndex(doc, [[[doc->sbApp selection] fields] objectAtIndex:1]);
+				doc->restoreFieldIdx = getEntryIndex(doc, [[[doc->sbApp selection] fields] objectAtIndex:0]);
 			} else {
 				doc->restoreCursorEnd = [[doc->sbApp selection] selectionEnd];
 			}