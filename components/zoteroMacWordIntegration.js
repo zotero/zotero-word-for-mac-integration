1d6da18442e5009ba54b5707639a13f359ad0845
Dan Stillman <dstillman@zotero.org>
2020-12-20 03:45:25 -0500

    Adjust URL for ARM warning

--- components/zoteroMacWordIntegration.js
+++ components/zoteroMacWordIntegration.js
@@ -284,7 +284,7 @@ function checkStatus(status, pre2016=false) {
 		else if (status === STATUS_EXCEPTION_ARM_NOT_SUPPORTED) {
 			let title = Zotero.getString('integration.error.incompatibleWordConfiguration');
 			let message = Zotero.getString('integration.error.armWordNotSupported', Zotero.appName);
-			let url = 'https://www.zotero.org/support/kb/mac_word_apple_silicon_not_supported';
+			let url = 'https://www.zotero.org/support/kb/mac_word_apple_silicon_compatibility';
 			let index = displayPrimaryMoreInformationAlert(title, message);
 			if (index == 0) {
 				Zotero.launchURL(url);
