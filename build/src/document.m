zotero/zotero-word-for-mac-integration

commit 0fcd1211270992c01c307746828526e2aec4330b

Fix notes losing formatting upon insert.
See zotero/zotero#2469

build/src/document.m
@@ -1129,11 +1129,8 @@ statusCode insertText(document_t *doc, const char htmlString[]) {
 	IGNORING_SB_ERRORS_BEGIN
 	// Save properties
 	WordFont* font = [selectionRange fontObject];
-	double oldFontSize = [font fontSize];
 	NSString* oldFontName = [font name];
 	NSString* oldFontOtherName = [font otherName];
-	WordE110 oldColorIndex = [font colorIndex];
-	WordE113 oldUnderline = [font underline];
 	IGNORING_SB_ERRORS_END
 	
 	// Insert a temp bookmark into which we'll insert the HTML
@@ -1167,11 +1164,8 @@ statusCode insertText(document_t *doc, const char htmlString[]) {
 	IGNORING_SB_ERRORS_BEGIN
 	// Set properties back to saved
 	font = [bookmarkRange fontObject];
-	[font setFontSize:oldFontSize];
 	[font setName:oldFontName];
 	[font setOtherName:oldFontOtherName];
-	[font setColorIndex:oldColorIndex];
-	[font setUnderline:oldUnderline];
 	IGNORING_SB_ERRORS_END
 	
 	// Remove the previously added empty paragraph

commit f1e3985d9252fc0078ebf345a567f9a54b5f649b

Fix citations inserted from notes having link appearance
Regression from 0fcd121

build/src/document.m
@@ -1143,6 +1143,8 @@ statusCode insertText(document_t *doc, const char htmlString[]) {
 	WordFont* font = [selectionRange fontObject];
 	NSString* oldFontName = [font name];
 	NSString* oldFontOtherName = [font otherName];
+	WordE110 oldColorIndex = [font colorIndex];
+	WordE113 oldUnderline = [font underline];
 	IGNORING_SB_ERRORS_END
 	
 	// Insert a temp bookmark into which we'll insert the HTML
@@ -1287,6 +1289,10 @@ statusCode convertPlaceholdersToFields(document_t *doc, const char* placeholders
 								 MAIN_FIELD_PREFIX,
 								 @"TEMP"];
 			[[link fieldCode] setContent:rawCode];
+			WordFont *font = [linkRange fontObject];
+			[font setColorIndex:WordE110Auto];
+			[font setUnderline:WordE113UnderlineNone];
+			
 			CHECK_STATUS_LOCKED(doc)
 			ENSURE_OK_LOCKED(doc, initField(doc, link, -1, -1, false, &newField))
 		}